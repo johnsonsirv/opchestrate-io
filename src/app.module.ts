945e521dd75d0adbbb28f0d6e973b349b52d9ab5
Victor Johnson <johnsonsirv@gmail.com>
2023-10-03 19:40:10 +0100

    set up typorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', //TODO: move to config b4 git commit
+      host: 'localhost',
+      port: 5432,
+      database: 'task-aide',
+      username: 'postgres',
+      password: 'postgres',
+      synchronize: true, //auto migrations, keep schemas in check
+      autoLoadEntities: true,
+    }),
+  ],
 })
 export class AppModule {}
