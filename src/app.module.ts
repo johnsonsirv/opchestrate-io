99b752ff06d2115c36cb58f3e67af08292011eae
Victor Johnson <johnsonsirv@gmail.com>
2023-10-04 00:47:32 +0100

    clean up configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.STAGE}`],
+    }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres', //TODO: move to config. Bad practice, Just temporal.
-      host: 'localhost',
-      port: 5432,
-      database: 'task-aide',
-      username: 'postgres',
-      password: 'postgres',
-      synchronize: true, //auto migrations, keep schemas in check
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        synchronize: true, //auto migrations, keep schemas in check
+        autoLoadEntities: true,
+        host: configService.get('DB_HOST'),
+        database: configService.get('DB_NAME'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+      }),
     }),
   ],
 })
